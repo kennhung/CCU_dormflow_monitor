cbc4ea30416427a6d962a8567c678feb6e29418e
KennHuang <kennhung@users.noreply.github.com>
2019-12-31 13:41:13 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+import bs4
+from bs4 import BeautifulSoup
+
+def getCurrFlow(ip):
+    res = requests.get('https://netflow.dorm.ccu.edu.tw/flows/'+ ip)
+    res.encodeing = "utf8"
+    return BeautifulSoup(res.text, 'lxml).find_all('font')[0].text
+    
+if __name__ == "__main__":
+    targetIP = input(IP: )
+    print(getCurrFlow(targetIP))
